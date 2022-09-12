967135311619650177edbfb80e5082182ba3e2e1
Ramiro Filippi <ramirofilippi@gmail.com>
2022-09-12 12:57:39 -0300

    Lab06ConLogin

--- UI.Web/Login.aspx.cs
+++ UI.Web/Login.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Business.Entities;
+using Business.Logic;
 
 namespace UI.Web
 {
@@ -17,20 +19,37 @@ namespace UI.Web
         protected void btnIngresar_Click(object sender, EventArgs e)
         {
             //Valido nombre y contraseña
-            if (txtUsuario.Text.ToLower() == "admin" && this.txtClave.Text == "admin")
+            /*if (txtUsuario.Text.ToLower() == "admin" && this.txtClave.Text == "admin")
             {
                 Page.Response.Write("Ingreso OK");
             }
             else
             {
                 Page.Response.Write("Usuario y/o Contraseña incorrectos");
+            }*/
+            UsuarioLogic usuario = new UsuarioLogic();
+            List<Usuario> usr = usuario.GetAll();
+            bool encontrado = false;
+
+            foreach (Usuario u in usr)
+            {
+                if (u.Clave == this.txtClave.Text && u.NombreUsuario == this.txtUsuario.Text)
+                {
+                    Response.Redirect("~/Default.aspx?");
+                    encontrado = true;
+                    break;
+                }
+            }
+            if (!encontrado)
+            {
+                Page.Response.Write("<script>alert('El usuario y/o la contraseña son incorrectos');</script>");
             }
         }
 
         protected void lnkRecordarClave_Click(object sender, EventArgs e)
         {
             //Redirecciono a otra pagina que deberia existir y para mostrar el mensaje tener dicho elemento
-            Response.Redirect("~/Default.aspx?msj=Es Ud. un usuario muy descuidado, haga memoria");
+            Page.Response.Write("<script>alert('Es Ud. un usuario muy descuidado, haga memoria');</script>");
         }
     }
 }
